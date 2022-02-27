674e4efd2f2f713188263a77a6d3d4e3627e924a
yajat2402 <96002631+yajat2402@users.noreply.github.com>
2022-02-27 19:38:16 +0530

    Add files via upload

---
+++ Assignment 4/Assign4_Output.py
@@ -0,0 +1,69 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:\Users\yajat\OneDrive\Desktop\Python\Assignment 4\Q1.py ======
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+====== RESTART: C:\Users\yajat\OneDrive\Desktop\Python\Assignment 4\Q2.py ======
+Enter the size of pascals triangle 6
+USING FOR LOOP
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+
+
+USING WHILE LOOP
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+
+
+USING RECURSSIONS
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+====== RESTART: C:\Users\yajat\OneDrive\Desktop\Python\Assignment 4\Q3.py ======
+Enter 2 numbers: 15 4
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> All the values are non zero
+<c> Filtered out numbers that are greater than 4 are : [7, 7, 8, 8, 8, 9, 9]
+<d> Set: {8, 9, 7}
+<e> Immutable set: frozenset({8, 9, 7})
+<f> Hash value of the max value from the above set: 9
+
+====== RESTART: C:\Users\yajat\OneDrive\Desktop\Python\Assignment 4\Q4.py ======
+Object created
+The name of the student is Yajat  and SID is 21103041
+Object destroyed
+
+====== RESTART: C:\Users\yajat\OneDrive\Desktop\Python\Assignment 4\Q5.py ======
+(a) The updated salary of Yajat is 70000
+(b) Record of Varun deleted
+
+====== RESTART: C:\Users\yajat\OneDrive\Desktop\Python\Assignment 4\Q6.py ======
+Enter the word: 
+Ritik
+Enter a new meaningful word using the exact same letters to test your friendship: 
+Kriti
+Does the word makes sense?(y/Y or n/N )Y
+The friends pass the friendship test
---
+++ Assignment 4/Assign4_sol.py
@@ -0,0 +1,185 @@
+#Question 1
+def TowerOfHanoi(n , from_rod, to_rod, middle_rod):
+	if n == 0:
+		return
+	TowerOfHanoi(n-1, from_rod, middle_rod, to_rod)
+	print("Move disk",n,"from rod",from_rod,"to rod",to_rod)
+	TowerOfHanoi(n-1, middle_rod, to_rod, from_rod)
+n = 3
+TowerOfHanoi(n, 'A', 'C', 'B')
+
+#############################################
+
+#Question 2
+
+from math import factorial, remainder
+from tracemalloc import start
+n=int(input('Enter the size of pascals triangle '))
+
+print("USING FOR LOOP")
+
+for i in range(n):
+	for j in range(n-i+1):
+		print(end=" ") #for spacing
+
+	for j in range(i+1):
+		print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")	# nCr = n!/((n-r)!*r!)
+	print()	# for new line
+print("\n\n")
+
+print("USING WHILE LOOP")
+
+i=0
+while(i<n):
+    z=n-i+1
+    while(z>0):
+        print(end=" ")
+        z-=1
+    y=0
+    while(y<i+1):
+        print(factorial(i) // (factorial(y) * factorial(i - y)), end=" ")
+        y+=1
+    i+=1
+    print()
+print("\n\n")
+
+print("USING RECURSSIONS")
+
+def pascal_triangle(n,originalength=n):
+    if n == 0:
+        return
+    pascal_triangle(n-1,originalength)
+    #printing the spaces
+    print('  '*(originalength-n), end='')
+
+    #first number is always 1
+    start = 1
+    for i in range(1, n+1):
+
+        print(start, end ='   ')
+        
+        #using Binomial Coefficient
+        start = start * (n - i) // i
+    print()
+pascal_triangle(n)
+
+######################################
+
+#Question 3
+
+int1, int2 = map(int,input("Enter 2 numbers: ").split())
+Quotient = int1 // int2
+Remainder = int1 % int2
+
+#part <a>
+print("a. Checking if the quotient and remainder is a callable value or not.")
+print(callable(Quotient))
+print(callable(Remainder))
+
+#part <b>
+if (Quotient == 0):
+    print("<b> The quotient is zero")
+if (Remainder == 0):
+    print("<b> The reminder is zero")
+if (Quotient != 0 and Remainder != 0):
+    print("<b> All the values are non zero")
+
+#part <c>
+partClist = [Quotient + 4, Remainder + 4, Remainder + 5, Quotient + 5, Remainder + 5, Quotient + 6, Remainder + 6]
+
+result = []
+for i in range(len(partClist)):
+    if partClist[i] > 4:
+        result.append(partClist[i])
+print(f"<c> Filtered out numbers that are greater than 4 are : {result}")
+
+#part <d>
+setresult = set(result)
+print("<d> Set:",setresult)
+
+#part <e>
+immutableSet = frozenset(setresult) #frozen Set is used to make the set immutable
+print("<e> Immutable set:",immutableSet)
+
+#part <f>
+print("<f> Hash value of the max value from the above set:", hash(max(immutableSet)))
+
+######################################
+
+#Question 4
+
+class Student:
+    def __init__(self, student,roll_no):
+        self.name = student
+        self.roll_no = roll_no
+    
+    def __del__(self):
+        print("Object destroyed")
+
+student1 = Student("Yajat", 21103041)
+print("Object created")
+
+print("The name of the student is",student1.name," and SID is", student1.roll_no)
+
+del student1
+
+#######################################
+
+#Question 5
+
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary 
+#creating employee records
+employee1 = Employee("Yajat", 40000)
+employee2 = Employee("Jyoti", 50000)
+employee3 = Employee("Varun", 60000)
+
+#part <a> updating salary
+employee1.salary = 70000
+print("(a) The updated salary of",employee1.name,"is",employee1.salary)
+#part <b> deleting a record
+print("(b) Record of Varun deleted", end='')
+del employee3
+
+#########################################
+
+#Question 6
+
+#inputting a word from the first friend
+word =input("Enter the word: \n")
+word=word.lower()
+
+#inputting a meaningful word with the exact same letters
+testword = input("Enter a new meaningful word using the exact same letters to test your friendship: \n")
+testword=testword.lower()
+#defining dictionary
+def count_in_dict(word):
+    count = {}
+    list1 = list(word)
+    
+    n = len(list1)
+    for i in range(n):
+        if list1[i] in count:
+            count[list1[i]] += 1
+        else:
+            count[list1[i]] = 1
+    return count
+#shopkeeper's input to verify the word's meaning
+def userinput():
+    if count_in_dict(word) != count_in_dict(testword):
+        print("The letters aren't exact, friendship is fake")
+        return
+    ans = input("Does the word makes sense?(y/Y or n/N )")
+
+    if ans == 'y' or ans=='Y':
+        print("The friends pass the friendship test")
+    elif ans == 'n' or ans=='N':
+        print("The word doesn't have a meaning, friendship is fake")
+    else:
+        print("Invalid input,try again with y/Y or n/N ")
+        userinput()
+userinput()
+
+
